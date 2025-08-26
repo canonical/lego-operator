f4531daf906cf21f731c73523c9f1a06dddf371b
saltiyazan <yazan.salti.19@gmail.com>
2025-08-26 15:03:24 +0400

    test: port existing integration test to jubilant  (#80)
    Co-authored-by: James Garner <james.garner@canonical.com>

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,7 +3,10 @@
 # See LICENSE file for licensing details.
 
 import os
+import pathlib
+import typing
 
+import jubilant
 import pytest
 
 
@@ -17,6 +20,12 @@ def pytest_addoption(parser: pytest.Parser) -> None:
     """
     parser.addoption(
         "--charm_path", action="store", default=None, help="Path to the charm under test"
+    )
+    parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="keep temporarily-created models",
     )
 
 
@@ -33,3 +42,23 @@ def pytest_configure(config: pytest.Config) -> None:
         pytest.exit("The --charm_path option is required. Tests aborted.")
     if not os.path.exists(charm_path):
         pytest.exit(f"The path specified for the charm under test does not exist: {charm_path}")
+
+
+@pytest.fixture(scope="session")
+def charm_path(request: pytest.FixtureRequest) -> pathlib.Path:
+    path: str = request.config.getoption("--charm_path")  # type: ignore
+    return pathlib.Path(path).resolve()
+
+
+@pytest.fixture(scope="module")
+def juju(request: pytest.FixtureRequest) -> typing.Iterator[jubilant.Juju]:
+    keep_models = bool(request.config.getoption("--keep-models"))
+
+    with jubilant.temp_model(keep=keep_models) as juju:
+        juju.wait_timeout = 1000
+
+        yield juju  # run the test
+
+        if request.session.testsfailed:
+            log = juju.debug_log(limit=1000)
+            print(log, end="")
